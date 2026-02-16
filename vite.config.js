e632904e2ebce01c04ac88f4c9c49612510ae2ab
faizal-intech <faizal.intech@gmail.com>
2026-02-16 17:59:43 +0530

    ready for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Set the development server port
+    strictPort: true // Exit if the port is already in use, instead of trying the next available port
+  },
+  preview: {
+    port: 8080 // Set the production preview server port
+  }
+});
